6d69cb32b4228af6a39055255e4a814bf8c0cdd4
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-14 09:45:56 +0430

    Add project files.

---
+++ HttpCookie.WebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HttpCookie.WebApplication2.Global" Language="C#" %>
---
+++ HttpCookie.WebApplication2/cooki/cooki.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Web = System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web;
+
+namespace HttpCookie.WebApplication2.cooki
+{
+    public partial class cooki : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Cookies["userName"].Value = "Annathurai";
+            Response.Write(Request.Cookies["userName"].Value);
+            var c = HttpContext.Current.Request.Cookies["userName"].Value;
+            Response.Write(c);
+        }
+    }
+}
